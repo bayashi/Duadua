52900a52ffc692b7edc8ad1f88c6c2d0e89f64e4
bayashi <bayashi@cpan.org>
2020-01-19 01:16:49 +0900

    Split YandexBot

--- lib/Duadua/Parser/Bot/YandexBot.pm
+++ lib/Duadua/Parser/Bot/YandexBot.pm
@@ -1,4 +1,4 @@
-package Duadua::Parser::Browser::Yandex;
+package Duadua::Parser::Bot::YandexBot;
 use strict;
 use warnings;
 use Duadua::Util qw//;
@@ -6,13 +6,14 @@ use Duadua::Util qw//;
 sub try {
     my ($class, $d) = @_;
 
-    if ( index($d->ua, ' YaBrowser/') > -1 && index($d->ua, 'Mozilla/') > -1 ) {
+    if ( index($d->ua, '+http://yandex.com/bots') > -1 ) {
         my $h = {
-            name => 'Yandex Browser',
+            name   => 'Yandex Bot',
+            is_bot => 1,
         };
 
         if ($d->opt_version) {
-            my ($version) = ($d->ua =~ m! YaBrowser/([\d.]+)!);
+            my ($version) = ($d->ua =~ m!compatible; [^/]+/([\d.]+); \+http://yandex!);
             $h->{version} = $version if $version;
         }
 
