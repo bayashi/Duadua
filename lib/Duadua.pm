6deef44ef6632cbdcbc76c7e0474790b9e0a49d4
bayashi <bayashi@cpan.org>
2020-03-10 21:59:58 +0900

    Support Sakura Cloud Simplemon

--- lib/Duadua.pm
+++ lib/Duadua.pm
@@ -24,6 +24,7 @@ my @PARSER_PROC_LIST = qw/
     Duadua::Parser::Browser::AppleSafari
     Duadua::Parser::Browser::Vivaldi
 
+    Duadua::Parser::Bot::Sakura
     Duadua::Parser::HTTPClient::HTTPClient
 
     Duadua::Parser::Bot::Twitterbot
