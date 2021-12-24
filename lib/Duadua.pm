f6d2d9ef039bcab394ca699c3d7f15ae1405d07a
bayashi <bayashi@cpan.org>
2021-12-24 20:37:06 +0000

    Add Attacker class for the UA string of Log4J CVE-2021-45046

--- lib/Duadua.pm
+++ lib/Duadua.pm
@@ -42,6 +42,7 @@ my @PARSER_PROC_LIST = qw/
     Duadua::Parser::Bot::Applebot
     Duadua::Parser::Bot::Msnbot
     Duadua::Parser::Bot::BotMisc
+    Duadua::Parser::Bot::Attacker
 
     Duadua::Parser::Browser::GoogleApp
     Duadua::Parser::Browser::SamsungInternetBrowser
